d0f87fdc6d9d835eb66ccd724c7257db6dada55b
mtrznadel24 <maciej.trznadel24@gmail.com>
2026-05-03 17:57:06 +0200

    feat: add Pydantic schemas for data validation

---
+++ backend/app/schemas.py
@@ -0,0 +1,36 @@
+from pydantic import BaseModel, Field
+from typing import List, Optional
+from enum import Enum
+
+
+class PhysicalTypeEnum(str, Enum):
+    screenless = "screenless"
+    modular = "modular"
+    newfm = "newfm"
+    next = "next"
+    other = "other"
+
+
+class ParcelLockerCreate(BaseModel):
+    name: str
+    city: Optional[str] = None
+    address: Optional[str] = None
+    description: Optional[str] = None
+    image_url: Optional[str] = None
+
+    status: str
+    physical_type: Optional[str] = "other"
+    is_24_7: bool = True
+    easy_access_zone: bool = False
+    payment_available: bool = False
+    functions: List[str] = []
+
+    longitude: float
+    latitude: float
+
+
+class ParcelLockerResponse(ParcelLockerCreate):
+    id: int
+
+    class Config:
+        from_attributes = True
