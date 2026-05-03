f5c67bb5c7875fbb7a0c0c0fd8685c9390b70b24
mtrznadel24 <maciej.trznadel24@gmail.com>
2026-05-04 01:55:40 +0200

    feat: setup redis with worker in docker compose

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -7,10 +7,16 @@ class Settings(BaseSettings):
     POSTGRES_DB: str
     POSTGRES_HOST: str = "localhost"
     POSTGRES_PORT: int = 5432
+    REDIS_HOST: str = "localhost"
+    REDIS_PORT: int = 6379
 
     @property
     def async_database_url(self) -> str:
         return f"postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
+
+    @property
+    def redis_url(self) -> str:
+        return f"redis://{self.REDIS_HOST}:{self.REDIS_PORT}/0"
 
     model_config = SettingsConfigDict(
         env_file=("../.env", ".env"), env_file_encoding="utf-8", extra="ignore"
