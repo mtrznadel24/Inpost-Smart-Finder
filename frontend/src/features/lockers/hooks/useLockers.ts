f9b828f084fb2d5c789c233343b000c5aef76f8e
mtrznadel24 <maciej.trznadel24@gmail.com>
2026-05-05 16:29:11 +0200

    feat: fix build

--- frontend/src/features/lockers/hooks/useLockers.ts
+++ frontend/src/features/lockers/hooks/useLockers.ts
@@ -1,6 +1,6 @@
 import { useQuery, keepPreviousData } from '@tanstack/react-query';
 import { getLockers } from '../api/getLockers';
-import { MapBounds } from '../types';
+import type { MapBounds } from '../types';
 import {lockerKeys} from "@/features/lockers/queryKeys.ts";
 
 export function useLockers(bounds: MapBounds | null) {
