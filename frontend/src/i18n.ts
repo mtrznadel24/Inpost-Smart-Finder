23d4e6820437864ec3ed501b2a82ce5637be0994
mtrznadel24 <maciej.trznadel24@gmail.com>
2026-05-06 20:26:56 +0200

    feat: add polish language to app and make it deafult

--- frontend/src/i18n.ts
+++ frontend/src/i18n.ts
@@ -2,20 +2,23 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 
 import translationEN from "./locales/en/translation.json";
+import translationPL from "./locales/pl/translation.json";
 
 const resources = {
   en: {
     translation: translationEN,
   },
+  pl: {
+    translation: translationPL,
+  }
 };
 
 i18n
   .use(initReactI18next)
   .init({
     resources,
-    lng: "en",
+    lng: "pl",
     fallbackLng: "en",
-
     interpolation: {
       escapeValue: false,
     },
